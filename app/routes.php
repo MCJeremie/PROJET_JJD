b2d2864fb5dc52b52fc14ca24b0d58a5e3829f18
Webforce3 <denis.nerborac@wf3.fr>
2016-06-08 18:00:14 +0200

    Update routes + navbar/sidebar

--- app/routes.php
+++ app/routes.php
@@ -1,14 +1,15 @@
 <?php
 
 $w_routes = array(
+	['GET', '/404', 'Default#page404', '404'],
 	['GET', '/', 'Default#home', 'home'],
 
-	['GET', '/movie/catalog', 'Movie#catalog', 'movie_catalog'],
 	['GET', '/movie/view/[i:id]', 'Movie#view', 'movie_view'],
-	['GET', '/movie/random', 'Movie#random', 'movie_random'],
-	['GET', '/movie/search/[:search]', 'Movie#search', 'movie_search_results'],
-	['GET', '/movie/search/', 'Movie#search', 'movie_search'],
-	['GET|POST', '/movie/add', 'Movie#add', 'movie_add'],
+	['GET', '/movie/catalog', 'Movie#catalog', 'movie_catalog', 'nav_title' => 'Liste des films'],
+	['GET', '/movie/random', 'Movie#random', 'movie_random', 'nav_title' => 'Film aléatoire'],
+	['GET', '/movie/search/[:search]', 'Movie#search', 'movie_search_results', 'nav_title' => 'Résultats de recherche'],
+	['GET', '/movie/search', 'Movie#search', 'movie_search', 'nav_title' => 'Recherche'],
+	['GET|POST', '/movie/add', 'Movie#add', 'movie_add', 'nav_title' => 'Ajouter un film'],
 
 	['GET', '/admin', 'Admin#dashboard', 'admin'],
 	['GET', '/admin/dashboard', 'Admin#dashboard', 'admin_dashboard'],
--- app/templates/admin/partials/sidebar.php
+++ app/templates/admin/partials/sidebar.php
@@ -1,8 +1,21 @@
 			<div class="col-sm-3 col-md-2 sidebar">
 				<ul class="nav nav-sidebar">
-					<li class="active"><a href="#">Overview <span class="sr-only">(current)</span></a></li>
-					<li><a href="#">Reports</a></li>
-					<li><a href="#">Analytics</a></li>
-					<li><a href="#">Export</a></li>
+
+					<?php
+					foreach($w_routes as $route) {
+
+						if (empty($route['nav_title'])) {
+							continue;
+						}
+
+						$route_name = $route[3];
+
+						$active = false;
+						if ($_SERVER['REQUEST_URI'] == $this->url($route_name)) {
+							$active = true;
+						}
+					?>
+					<li class="<?= $active ? 'active' : '' ?>"><a href="<?= $this->url($route_name) ?>"><?= $route['nav_title'] ?></a></li>
+					<?php } ?>
 				</ul>
 			</div>
